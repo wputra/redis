1a35d5fe7ad05e8d95f505e03d8a3fa2222b9ce6
wputra <wasis.putra@gmail.com>
2016-04-04 15:06:30 +0700

    set net.core.somaxconn=65535
    https://github.com/docker/docker/issues/4611

--- 3.0/docker-entrypoint.sh
+++ 3.0/docker-entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 
+sysctl -w net.core.somaxconn=65535
+
 # allow the container to be started with `--user`
 if [ "$1" = 'redis-server' -a "$(id -u)" = '0' ]; then
 	chown -R redis .
