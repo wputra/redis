d05981f26025252dbbabcb26a0d9d4209232e88e
wputra <wasis.putra@gmail.com>
2016-04-04 18:54:19 +0700

    add redis.conf

--- 3.0/docker-entrypoint.sh
+++ 3.0/docker-entrypoint.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 set -e
 
-sysctl -w net.core.somaxconn=65535
-
 # allow the container to be started with `--user`
 if [ "$1" = 'redis-server' -a "$(id -u)" = '0' ]; then
+	sysctl -p
 	chown -R redis .
 	exec gosu redis "$BASH_SOURCE" "$@"
 fi
